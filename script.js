cf3f1a4484b3290627d93ade628b6dc0ba6845c2
unknown <manirumva@gmail.com>
2023-08-01 11:33:51 +0200

    Updated script.js file

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const hamburger = document.querySelector('.btn');
 const navMenu = document.querySelector('.menu-sec');
 
-
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');  
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
-  
 
 document.querySelectorAll('.menu-sec').forEach((n) => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');  
-  }));  
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
